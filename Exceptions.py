54fdff5d7d6f824a2f999adb4cf709fd9a35780d
Dolev Zohar <zohar.dolev7@gmail.com>
2025-02-21 10:46:56 +0200

    Contiued working on the dashboard. Also added a file where classes regrarding exceptions will be dealt with. To do: Continue working on the dashboard. Create new exceptions to fit the code Add more parameters to the check (namely LCP, CLS, INP)

---
+++ Exceptions.py
@@ -0,0 +1,1 @@
+class CpuTimeException(Exception):
--- TempDashboard.py
+++ TempDashboard.py
@@ -1,5 +1,7 @@
 from metrics import Metrics
 import sqlite3
+import requests
+from urllib.parse import urlparse
 
 def initializeDatabase():
     """Creates the SQLite database and urls table if they don't exist."""
@@ -15,7 +17,7 @@ def initializeDatabase():
     conn.close()
 
 def chooseUrl():
-    maxId=0
+    maxId=1
     conn = sqlite3.connect("url.db")
     cursor = conn.cursor()
     table = cursor.fetchall()
@@ -24,9 +26,49 @@ def chooseUrl():
         ++maxId
 
     id = input("Enter desired id")
-    if id>maxId
+    if id>maxId:
         raise Exception("There is no url with such id")
-    if id<0
-        raise Exception("Id must not be negative")
+    if id<1:
+        raise Exception("Id must be at least 1")
+
+    return table[id]
+
+def ValidateUrl(url):
+    if not urlparse.scheme(url):
+        url = 'http://' + url
+    return url
+
+def CheckUrlIntegrity(url):
+    url = ValidateUrl(url)
+
+    try:
+        response = requests.get(url)
+        # If the status code is 200, the URL is reachable
+        if response.status_code == 200:
+            print("URL is valid and reachable.")
+            return True
+        else:
+            print(f"URL returned status code: {response.status_code}")
+            return False
+    except requests.exceptions.RequestException as e:
+        # Handling errors
+        print(f"An error occurred: {e}")
+        return False
+
+def getUrl():
+    url = input("Enter your desired url")
+    if (CheckUrlIntegrity(url))
+        GetDesiredVariables(ValidateUrl(url))
+    else:
+        print("Invalid url")
+    return
+
+def GetDesiredVariables(url):
+    finished = false
+    while not finished:
+        try:
+            cpu_time_weight = input("Enter weight for cpu_time")
+            if (cpu_time_weight<0)
+                raise
 
 def getStatistics(url):
